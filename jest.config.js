ef194119f2c68724e47513757cda73d9316e4f0d
Tom Gropper <tomgropper@Toms-MacBook-Pro.local>
2026-02-18 21:46:19 +0200

    fix npm test

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,15 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   testEnvironment: "jsdom",
+  preset: 'ts-jest',
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.module\\.css$': '<rootDir>/__mocks__/styleMock.js',
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
   },
--- pages/api/hello.js
+++
@@ -1,4 +0,0 @@
-// Placeholder file to satisfy Next.js type checking
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello" });
-}
